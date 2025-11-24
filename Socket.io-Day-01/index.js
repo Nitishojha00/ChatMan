ee5959f3d6de88229dea1d19e3c1ba1cbc3442ef
Nitishojha00 <nitishojha00@gmail.com>
2025-11-24 20:38:01 +0530

    started working on 2 major project also

---
+++ Socket.io-Day-01/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express()
+const { Server } = require("socket.io");
+const http = require('http')
+
+const server = http.createServer(app);
+const io = new Server(server);
+server.listen(4000,()=>{
+    console.log("Server is running on port 4000");
+})
+
+// yejo server bana isko upgrade krke web socket m bana padega
+// normal request upar handle hojayega web socket wala ab niche handle hogi
+
+io.on("connection",(socket)=>{
+
+    socket.on('message',(data)=>{
+        io.emit('new-message',data);   // mtlb sbko ye message bhejrha  emit mtlb bhejna
+    })
+
+    socket.on("disconnect",()=>{
+        console.log("Disconnected From Server");  // server ko aisa disconnect krta hai
+    })
+})
